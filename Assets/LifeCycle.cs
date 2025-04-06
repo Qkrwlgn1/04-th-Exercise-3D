eeac8e3af3f68888b58fdb02013353e28ee32073
박지후 <jihoopark0725@gmail.com>
2025-04-06 15:52:31 +0900

    FirstGit

---
+++ Assets/LifeCycle.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class LifeCycle : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            Debug.Log("플레이어가 아무키를 눌렀습니다.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Player : Actor
+{
+    public string move()
+    {
+        return "플레이어는 움직입니다.";
+    }
+}
